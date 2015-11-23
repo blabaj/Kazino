9b9a69981f0269ba0596dbbc713e72e8f75545d9
blabaj <bajec.b@gmail.com>
2015-11-23 13:39:06 +0100

    Dodani šumniki

--- kazino.py
+++ kazino.py
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 skritoStevilo = 8
 vnos = 0
 konec = False
 nadaljevanje = True
-budget = int(raw_input("Vnesi koliko denarja zels porabiti. Za vsako igro avtomat pobere 1\xe2\x82\xac: "))
+budget = int(raw_input("Vnesi koliko denarja želš porabiti. Za vsako igro avtomat pobere 1\xe2\x82\xac: "))
 while budget != 0 and konec != True:
     while (vnos != skritoStevilo and konec != True):
         budget = budget - 1
-        vnos = int(raw_input("Vnesi skrito stevilo: "))
+        vnos = int(raw_input("Vnesi skrito število: "))
         if vnos == skritoStevilo:
-            print ("Cestitke zadeli ste 20\xe2\x82\xac!")
+            print ("Čestitke zadeli ste 20\xe2\x82\xac!")
             konec = True
             break
         elif vnos != skritoStevilo:
@@ -16,10 +17,10 @@ while budget != 0 and konec != True:
 
         while nadaljevanje == True:
                 if (budget > 0):
-                    print "Imate se " + str(budget) + "\xe2\x82\xac"
-                    nadaljuje = str(raw_input("Zelite nadaljevati (Da/Ne): ")).upper()
+                    print "Imate še " + str(budget) + "\xe2\x82\xac"
+                    nadaljuje = str(raw_input("Želite nadaljevati (Da/Ne): ")).upper()
                     if nadaljuje == "NE":
-                        print ("Vracamo vam preostanek denarja - " + str(budget) + "\xe2\x82\xac")
+                        print ("Vračamo vam preostanek denarja: " + str(budget) + "\xe2\x82\xac")
                         print ("Nasvidenje")
                         nadaljevanje = False
                         konec = True
@@ -33,4 +34,4 @@ while budget != 0 and konec != True:
                     konec = True
                     break
 
-                print "Napacen ukaz! Ali zelis nadaljevati (Da/Ne)"
+                print "Napačen ukaz! Ali želiš nadaljevati (Da/Ne)"
